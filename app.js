f855aa202c8e696473e31e8505aabd09442f7152
Raj Patel <rajp4690@gmail.com>
2019-02-01 23:39:53 -0500

    Connection to backend and authentication on frontend

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ const app = express();
 
 app.use(bodyparser.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if(req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 app.use(authentication);
 
 app.use('/graphql', graphqlHttp({
@@ -24,7 +34,7 @@ app.get('/', (req, res, next) => {
 });
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-im6qg.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`).then(() => {
-    app.listen(3000);
+    app.listen(8000);
 }).catch (err => {
     console.log(err); 
 });
