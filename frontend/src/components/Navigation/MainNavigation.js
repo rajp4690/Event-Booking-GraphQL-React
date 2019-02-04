174e018dbbd38d03a52490da4ab7d05a1e0968e1
Raj Patel <rajp4690@gmail.com>
2019-02-04 16:33:19 -0500

    Create event with authentication and fetch events

--- frontend/src/components/Navigation/MainNavigation.js
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -20,9 +20,15 @@ const mainNavigation = props => (
                             <li>
                                 <NavLink to="/events">Events</NavLink>
                             </li>
-                            {context.token && <li>
-                                <NavLink to="/bookings">Bookings</NavLink>
-                            </li>}
+                            {context.token && 
+                                <React.Fragment>
+                                <li>
+                                    <NavLink to="/bookings">Bookings</NavLink>
+                                </li>
+                                <li>
+                                    <button onClick={ context.logout }>Logout</button>
+                                </li>
+                                </React.Fragment>}
                         </ul>
                     </nav>
                 </header>
